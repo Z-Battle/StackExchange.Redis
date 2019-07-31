59d3f7bca5d99220fdd7e68f8c0b0b0c4f30a23f
Sylvain LEBEAUPIN <sjvl-z-battle@users.noreply.github.com>
2019-07-31 23:02:43 +0200

    Add InstanceDetails for Sentinel with UT.

---
+++ src/StackExchange.Redis/SentinelInstanceDetails.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace StackExchange.Redis
+{
+    public partial class ConnectionMultiplexer
+    {
+        internal class SentinelInstanceDetails
+        {
+
+            public string Name { get; }
+            public IPAddress Ip { get; }
+            public ushort Port { get; }
+
+            public string RunId { get; }
+            public string Flags { get; }
+
+            public int LinkPendingCommands { get; }
+            public int LinkReferenceCount { get; }
+            public int LastPingSent { get; }
+            public int LastOkPingReply { get; }
+            public int LastPingReply { get; }
+            public int DownAfterMilliseconds { get; }
+            public int InformationRefresh { get; }
+            public string RoleReported { get; }
+            public int RoleReportedTime { get; }
+            public int ConfigurationOfEpoch { get; }
+            public int NumberOfSlaves { get; }
+            public int NumberOfOtherSentinels { get; }
+            public int Quorum { get; }
+            public int FailoverTimeout { get; }
+            public int ParallelSynchronizations { get; }
+
+            internal SentinelInstanceDetails(IEnumerable<KeyValuePair<string, string>> kvps)
+            {
+                foreach (var kvp in kvps)
+                    switch (kvp.Key)
+                    {
+                        case @"name": this.Name = kvp.Value; break;
+                        case @"ip": if (IPAddress.TryParse(kvp.Value, out var ip)) this.Ip = ip; break;
+                        case @"port": if (ushort.TryParse(kvp.Value, out var port)) this.Port = port; break;
+                        case @"runid": this.RunId = kvp.Value; break;
+                        case @"flags": this.Flags = kvp.Value; break;
+                        case @"link-pending-commands": if (int.TryParse(kvp.Value, out var linkPendingCommands)) this.LinkPendingCommands = linkPendingCommands; break;
+                        case @"link-refcount": if (int.TryParse(kvp.Value, out var linkReferenceCount)) this.LinkReferenceCount = linkReferenceCount; break;
+                        case @"last-ping-sent": if (int.TryParse(kvp.Value, out var lastPingSent)) this.LastPingSent = lastPingSent; break;
+                        case @"last-ok-ping-reply": if (int.TryParse(kvp.Value, out var lastOkPingReply)) this.LastOkPingReply = lastOkPingReply; break;
+                        case @"last-ping-reply": if (int.TryParse(kvp.Value, out var lastPingReply)) this.LastPingReply = lastPingReply; break;
+                        case @"down-after-milliseconds": if (int.TryParse(kvp.Value, out var downAfterMilliseconds)) this.DownAfterMilliseconds = downAfterMilliseconds; break;
+                        case @"info-refresh": if (int.TryParse(kvp.Value, out var informationRefresh)) this.InformationRefresh = informationRefresh; break;
+                        case @"role-reported": this.RoleReported = kvp.Value; break;
+                        case @"role-reported-time": if (int.TryParse(kvp.Value, out var roleReportedTime)) this.RoleReportedTime = roleReportedTime; break;
+                        case @"config-epoch": if (int.TryParse(kvp.Value, out var configurationOfEpoch)) this.ConfigurationOfEpoch = configurationOfEpoch; break;
+                        case @"num-slaves": if (int.TryParse(kvp.Value, out var numberOfSlaves)) this.NumberOfSlaves = numberOfSlaves; break;
+                        case @"num-other-sentinels": if (int.TryParse(kvp.Value, out var numberOfOtherSentinels)) this.NumberOfOtherSentinels = numberOfOtherSentinels; break;
+                        case @"quorum": if (int.TryParse(kvp.Value, out var quorum)) this.Quorum = quorum; break;
+                        case @"failover-timeout": if (int.TryParse(kvp.Value, out var failoverTimeout)) this.FailoverTimeout = failoverTimeout; break;
+                        case @"parallel-syncs": if (int.TryParse(kvp.Value, out var parallelSynchronizations)) this.ParallelSynchronizations = parallelSynchronizations; break;
+                    }
+            }
+
+            public override string ToString() => $@"[{this.Ip}]:{this.Port} (""{this.Name}"" {this.Flags})";
+
+        }
+    }
+}
---
+++ tests/StackExchange.Redis.Tests/SentinelInstanceDetailsTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace StackExchange.Redis.Tests
+{
+    public class SentinelInstanceDetailsTests : TestBase
+    {
+
+        public SentinelInstanceDetailsTests(ITestOutputHelper output) : base(output) { }
+
+        [Fact]
+        public void SentinelInstanceDetailsTest()
+        {
+            var kvps =
+                new Dictionary<string, string>()
+                {
+                    { "name" , "mymaster"},
+                    { "ip" , "127.0.0.1"},
+                    { "port" , "6379"},
+                    { "runid" , "953ae6a589449c13ddefaee3538d356d287f509b"},
+                    { "flags" , "master"},
+                    { "link-pending-commands" , "0"},
+                    { "link-refcount" , "1"},
+                    { "last-ping-sent" , "0"},
+                    { "last-ok-ping-reply" , "735"},
+                    { "last-ping-reply" , "735"},
+                    { "down-after-milliseconds" , "5000"},
+                    { "info-refresh" , "126"},
+                    { "role-reported" , "master"},
+                    { "role-reported-time" , "532439"},
+                    { "config-epoch" , "1"},
+                    { "num-slaves" , "1"},
+                    { "num-other-sentinels" , "2"},
+                    { "quorum" , "2"},
+                    { "failover-timeout" , "60000"},
+                    { "parallel-syncs" , "1"},
+                };
+            var instanceDetails = new ConnectionMultiplexer.SentinelInstanceDetails(kvps);
+            Assert.Equal("mymaster", instanceDetails.Name);
+            Assert.Equal("127.0.0.1", instanceDetails.Ip.ToString());
+            Assert.Equal(6379, instanceDetails.Port);
+            Assert.Equal("953ae6a589449c13ddefaee3538d356d287f509b", instanceDetails.RunId);
+            Assert.Equal("master", instanceDetails.Flags);
+            Assert.Equal(0, instanceDetails.LinkPendingCommands);
+            Assert.Equal(1, instanceDetails.LinkReferenceCount);
+            Assert.Equal(0, instanceDetails.LastPingSent);
+            Assert.Equal(735, instanceDetails.LastOkPingReply);
+            Assert.Equal(735, instanceDetails.LastPingReply);
+            Assert.Equal(5000, instanceDetails.DownAfterMilliseconds);
+            Assert.Equal(126, instanceDetails.InformationRefresh);
+            Assert.Equal("master", instanceDetails.RoleReported);
+            Assert.Equal(532439, instanceDetails.RoleReportedTime);
+            Assert.Equal(1, instanceDetails.ConfigurationOfEpoch);
+            Assert.Equal(1, instanceDetails.NumberOfSlaves);
+            Assert.Equal(2, instanceDetails.NumberOfOtherSentinels);
+            Assert.Equal(2, instanceDetails.Quorum);
+            Assert.Equal(60000, instanceDetails.FailoverTimeout);
+            Assert.Equal(1, instanceDetails.ParallelSynchronizations);
+        }
+
+    }
+}
